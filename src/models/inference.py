b2f282786c1e201f165246dc927d5924efd72897
Ryan Culkin <ryanculkin@gmail.com>
2018-12-06 21:42:42 -0500

    add gpu for inference

--- src/models/inference.py
+++ src/models/inference.py
@@ -1,3 +1,4 @@
+import argparse
 import sys; sys.path.append("..")
 import pickle 
 import subprocess
@@ -14,10 +15,10 @@ import numpy as np
 
 def angle(y_pred: Tuple[th.Tensor], y: None):
     pred,reference,beta = y_pred
-    cosine_sim = nn.CosineSimilarity(dim=0)(pred, y - reference)
+    cosine_sim = nn.CosineSimilarity(dim=0)(pred, y)
     return th.mean(cosine_sim, dim=0)
 
-def get_device(use_gpu=False):
+def get_device(use_gpu):
     if use_gpu:
         try:
             output = subprocess.check_output("free-gpu", shell=True)
@@ -31,10 +32,22 @@ def get_device(use_gpu=False):
             device = None
     else:
         device = None
+
+    if device is not None:
+        device = th.device(device)
     return device
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--use-gpu", action='store_true')
+    parser.add_argument("--model-path", required=True)
+    args = parser.parse_args()
+
+    use_gpu = args.use_gpu
+    model_path = args.model_path
+
+
     # load vocab from pickled dict
     str_to_ids = "../../data/embeddings/str_to_ids.pkl"
     ids_to_str  = "../../data/embeddings/ids_to_str.pkl"
@@ -60,22 +73,33 @@ if __name__ == '__main__':
         embedding_arr[idx,:] = corresponding_embedding
 
 
-    # cpu for now...
-    device = get_device(use_gpu=False)
+    
+    device = get_device(use_gpu)
+    batch_size = 2048
     model = ColorNet(color_dim=3, vocab=str_to_ids, pretrained_embeddings=embedding_arr, beta=0.3/0.7, device=device)
-    test_dl = DataLoader("../../data/raw/xkcd_colordata", "../../data/raw/", "test")
+    model = model.cuda(device)
+    test_dl = DataLoader("../../data/raw/xkcd_colordata", "../../data/raw/", "test", batch_size=batch_size, device=device)
 
-    count = 0.0
+    # import pdb;pdb.set_trace()
+    batch_count = 0
+    inst_count = 0
     cos_accum = 0.0
     for inst in test_dl:
+
+        # the dataloader should already do this...?
+        # but it complains if we don't do this
+        inst = [_.cuda(device) for _ in inst]
+
         gold = inst[2]
         result = model(inst)
         cos_accum += angle(result, gold)
-        count += 1.0
-        if count % 10 == 0:
-            print(count)
-    print (count)
-    print(cos_accum / count)
+
+        batch_count += 1
+        inst_count += batch_size
+        if batch_count % 10 == 0:
+            print('batch_count:%d, inst_count:%d, avg_cosine:%.3f' % (batch_count, inst_count, cos_accum / batch_count))
+
+    print(cos_accum / batch_count)
 
 
 
